bdd494a6734a8934806968cb27d97e3bdee1ce77
Sai Charan <mscharan2807@gmail.com>
2026-06-16 21:16:19 +0530

    Updates

---
+++ assets/Skeleton-C62tEu30.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Cmqo0bGj.js";function n({className:e=""}){return t.jsx("div",{className:`animate-pulse rounded-lg bg-muted ${e}`})}export{n as S};
--- assets/Skeleton-Du75BzWl.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DgFFEz_7.js";function n({className:e=""}){return t.jsx("div",{className:`animate-pulse rounded-lg bg-muted ${e}`})}export{n as S};
